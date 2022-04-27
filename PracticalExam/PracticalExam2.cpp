8025ecf8bb05c446d7b0d1cbccf1b74b16d89058
Hunghh90 <hungbkvn9x1@gmail.com>
2022-04-27 19:32:29 +0700

    Update PracticalExam2.cpp

--- PracticalExam/PracticalExam2.cpp
+++ PracticalExam/PracticalExam2.cpp
@@ -2,8 +2,10 @@
 #include "reverseArray.h"
 int main(){
 	int x;
-	printf("Nhap x:");
-	scanf("%d",&x);
+	do{
+		printf("Nhap x:");
+		scanf("%d",&x);
+	}while(x<=0);
 	int arr[x];
 	nhapMang (arr,x);
 	inNguoc (arr,x);	
